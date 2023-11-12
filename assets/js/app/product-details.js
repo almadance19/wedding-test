8a0ea97f13ae21d253813d912fddefa385b96fcf
almadance19 <132331786+almadance19@users.noreply.github.com>
2023-11-12 16:46:49 +0100

    commit message

--- assets/js/app/product-details.js
+++ assets/js/app/product-details.js
@@ -3,9 +3,7 @@ const products= [];
 const url_version ="https://script.google.com/macros/s/AKfycbz70KCClkMxIwfMWYlzcZ8BqVgzQtWu03Xa-EhmB5R91OeRulnFudVrl7O6_oTNi9hj/exec";
 
 
-const vimeoIframe = document.getElementById('iframe_vimeo');
-
- window.addEventListener('DOMContentLoaded', getProductData);
+window.addEventListener('DOMContentLoaded', getProductData);
 
 // function getInit() {
 //   //initPayPalButton();
@@ -23,21 +21,21 @@ function getProductData() {
    if (extractedProduct) {
        console.log('Got the id - ' + extractedProduct.id);
 
-       vimeoIframe.src =extractedProduct.link_preview+"?badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479" 
    
     } else {
         console.log('Could not find id.');
 
+             //vimeoIframe.src =extractedProduct.link_preview+"?badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479" 
             // Creating iframe element 
-            var el = document.createElement("iframe");
-            el.src ="https://vimeo.com/883720173?"+"badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479"  
-            // Setting the values for the attributes 
-            //el.srcdoc = `<h1>.Iframe Element.</h1> <p> Hello Geek! <br> How are you? </p>`;
-            el.style = "position:absolute;top:0;left:0;width:100%;height:100%;"; 
-            el.allow ="autoplay; fullscreen; picture-in-picture";
-            el.frameborder="0";
-            el.width = "400px"; 
-            el.height = "200px"; 
+            // var el = document.createElement("iframe");
+            // el.src ="https://vimeo.com/883720173?"+"badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479"  
+            // // Setting the values for the attributes 
+            // //el.srcdoc = `<h1>.Iframe Element.</h1> <p> Hello Geek! <br> How are you? </p>`;
+            // el.style = "position:absolute;top:0;left:0;width:100%;height:100%;"; 
+            // el.allow ="autoplay; fullscreen; picture-in-picture";
+            // el.frameborder="0";
+            // el.width = "400px"; 
+            // el.height = "200px"; 
         
             // Adding the created iframe to div as 
             // a child element 
