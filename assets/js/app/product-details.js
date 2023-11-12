284c00aea0f466978a5fe5a6fa8f4436bc45730f
almadance19 <132331786+almadance19@users.noreply.github.com>
2023-11-12 15:02:53 +0100

    commit message

--- assets/js/app/product-details.js
+++ assets/js/app/product-details.js
@@ -1,12 +1,7 @@
 const url_products ='https://script.google.com/macros/s/AKfycbyh4-pVuUIwEIP1EeS2u00TXcEW8Bp_Vw2-GZ6h92UIp-FHaT9Wm0c7T_NqklpGATm8/exec';
 const products= [];
-const listElement = document.getElementById('add-products-row');
-const postTemplate = document.getElementById('single-post');
 const url_version ="https://script.google.com/macros/s/AKfycbz70KCClkMxIwfMWYlzcZ8BqVgzQtWu03Xa-EhmB5R91OeRulnFudVrl7O6_oTNi9hj/exec";
-let old_db_version; 
-///console.log(old_db);
-//let db = openRequest.result;
-//db.close();
+
 
 
 
